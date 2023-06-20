b3747607926a9be21b7f90f59e8143fff44780e9
ericj <ericjuncheng10@gmail.com>
2023-06-20 20:52:52 +0800

    v2.1

--- public/Assests/JS/profile.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const profile_photo_form = document.querySelector('#profile_photo_form');
-profile_photo_form.addEventListener('submit',async(e)=>{
-    e.preventDefault();
-    
-})
